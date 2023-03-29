a987bb6966abdd1b591cf20692dd59c2ac5964be
Mahdi Bahar <mahdi.bahar.g@gmail.com>
2023-03-29 16:22:33 +0430

    End of practice

--- Python/Exercise 12/numpy_practice.py
+++ Python/Exercise 12/numpy_practice.py
@@ -56,3 +56,88 @@ print(arr_3D.ndim)
 print(arr.shape)
 print(arr_2D.shape)
 print(arr_3D.shape)
+
+
+######
+#zero array
+arr_zero = np.zeros((2,3),dtype= np.int32)
+print(arr_zero)
+
+#####
+# one array
+arr_one = np.ones((2,3,4),dtype= np.int32)
+print(arr_one)
+
+#arange
+
+a = np.arange(1, 10,2)
+print(a)
+
+#linspace
+b= np.linspace(1,2,5)
+print(b)
+
+
+#indexing
+
+arr_id = np.arange(1,9)
+print(arr_id)
+print(type(arr_id))
+
+print(arr_id[4])
+
+## 2D
+arr_2d = np.array([
+    
+    [1,2,3],
+    [4,5,6]
+    
+    ])
+
+print(arr_2d.shape)
+print(arr_2d[0])
+print(arr_2d[1,1])
+
+## 3D
+
+arr_3d = np.array([
+    [
+     [1,2],[3,4],[5,6]
+     ],
+    [
+     [7,8],[9,10],[11,12]
+     ]
+    ])
+
+print(arr_3d.shape)
+print(arr_3d[1,2,0])
+
+## slicing
+
+#2D
+arr_2d_slciing = arr_2d[:1,1:]
+print(arr_2d_slciing)
+
+
+## View , Copy
+
+a= np.array(
+    [[80,81,82],
+    [83,84,85]
+    ]
+    )
+b=a[0:,0:2]
+c=a[0:,0:2].copy()
+
+c[0,0]=0
+
+print(b)
+
+print(c)
+
+print(a)
+
+### np.add
+### np.subtract
+### np.multiply
+### np.divide
