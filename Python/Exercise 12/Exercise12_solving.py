c5320e08f47b7e5e2750d198437f527bf0c23ee6
Mahdi Bahar <mahdi.bahar.g@gmail.com>
2023-03-29 16:52:28 +0430

    Srart: Solving Exercise 12

---
+++ Python/Exercise 12/Exercise12_solving.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 15:43:06 2023
+
+@author: Mahdi Bahar
+
+Solving the Exersice 12
+
+Numpy
+"""
+
+#Exercise 1
+
+
+import numpy as np
+
+a = np.zeros((4,5,3))
+print(a)
+
+print(a.shape)
